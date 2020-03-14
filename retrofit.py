55bd5a541309b0ca471750889378316adc0d5c93
krebman420 <ethancf1999@gmail.com>
2020-03-14 16:38:06 -0400

    small fixes

--- retrofit.py
+++ retrofit.py
@@ -2,10 +2,11 @@ import time, os
 
 path = os.path.join("home","Shigure","discordpy-shigubot")
 
+os.system("cd")
 os.system("cd " + path)
 os.system("git pull https://github.com/ecfidler/discordpy-shigubot.git")
 time.sleep(5)
 
 os.system("ls")
-os.system("python3 " + path + "/shigubot.py &")
+os.system("python3 -m " + path + "/shigubot.py &")
 print("cloned")
