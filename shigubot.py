0b0ef0b22f678fddb0abc3fe3f864cffa7efb015
krebman420 <ethancf1999@gmail.com>
2020-03-16 17:30:47 -0400

    spicy testing

--- shigubot.py
+++ shigubot.py
@@ -194,27 +194,35 @@ async def on_raw_reaction_add(payload):
             except:
                 await msg.channel.send("No image found in message")
 
-'''
+    sauce_messages = ["earned 3 michelin stars!","was some good shit"]
 
-if payload.emoji.name == "": #chef's choice mild
-        checkSauce(await client.get_channel(payload.channel_id).fetch_message(payload.message_id))
-        async with client.get_channel(payload.channel_id).typing():
-            for role in client.get_guild(payload.guild_id).get_member(payload.user_id).roles:
-                if role.id == 676571207323090944:
-                    msg = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
-                    await client.get_channel(592222434396995604).send(embed=transcribe(msg,client.get_guild(payload.guild_id).get_member(payload.user_id).display_name))
-                    await client.get_channel(payload.channel_id).send("Image sent to chef's choice mild!")
+    '''
 
-if payload.emoji.name == "": #chef's choice spicy
-        checkSauce(await client.get_channel(payload.channel_id).fetch_message(payload.message_id))
-        async with client.get_channel(payload.channel_id).typing():
-            for role in client.get_guild(payload.guild_id).get_member(payload.user_id).roles:
-                if role.id == 676571207323090944:
-                    msg = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
-                    await client.get_channel(592225505592344577).send(embed=transcribe(msg,client.get_guild(payload.guild_id).get_member(payload.user_id).display_name))
-                    await client.get_channel(payload.channel_id).send("Image sent to chef's choice spicy!")
+    if payload.emoji.name == "": #chef's choice mild
+            checkSauce(await client.get_channel(payload.channel_id).fetch_message(payload.message_id))
+            async with client.get_channel(payload.channel_id).typing():
+                for role in client.get_guild(payload.guild_id).get_member(payload.user_id).roles:
+                    if role.id == 676571207323090944:
+                        msg = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
+                        await client.get_channel(592222434396995604).send(embed=transcribe(msg,client.get_guild(payload.guild_id).get_member(payload.user_id).display_name))
+                        await client.get_channel(payload.channel_id).send("Image sent to chef's choice mild!")
+    '''
+    if payload.emoji.name == "🔴": #chef's choice spicy
+            checkSauce(await client.get_channel(payload.channel_id).fetch_message(payload.message_id))
+            async with client.get_channel(payload.channel_id).typing():
+                for role in client.get_guild(payload.guild_id).get_member(payload.user_id).roles:
+                    if role.id == 676571207323090944:
+                        
+                        msg = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
+                        outString = msg.author.mention + "\'s post in <#" + str(msg.channel.id) + "> " + random.choice(sauce_messages)
+                        await msg.attachments[-1].save(os.path.join(source_path,'images','chefchoice.jpg'))
+                        await client.get_channel(592225505592344577).send(outString,file=discord.File(os.path.join(source_path,'images','chefchoice.jpg')))
+                        '''
+                        await client.get_channel(592225505592344577).send(embed=transcribe(msg,client.get_guild(payload.guild_id).get_member(payload.user_id).display_name))
+                        await client.get_channel(payload.channel_id).send("Image sent to chef's choice spicy!")
+                        '''
 
-'''
+
 
 @client.event
 async def on_member_join(member):
